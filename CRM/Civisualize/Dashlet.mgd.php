22662d88878b2bb0d1362ac49ca4e914d030a446
Xavier Dutoit <xavier@tttp.eu>
2014-10-05 12:02:25 +0200

    add example of dc syntax for bar+pie chart

---
+++ CRM/Civisualize/Dashlet.mgd.php
@@ -0,0 +1,35 @@
+<?php
+
+return array (
+0=>array (
+  'name'=>'dataviz_contact',
+  'entity'=> 'Dashboard',
+  'params'=> array (
+    'version'=>'3',
+    'name'=>'dataviz_contact',
+    'label'=> 'Dataviz of contacts',
+    'url' => CRM_Utils_System::url('civicrm/dataviz/contact', 'snippet=4'),
+  )
+),
+1=>array (
+  'name'=>'dataviz_events',
+  'entity'=> 'Dashboard',
+  'params'=> array (
+    'version'=>'3',
+    'name'=>'dataviz_event',
+    'label'=> 'Dataviz of events',
+    'url' => CRM_Utils_System::url('civicrm/dataviz/events', 'snippet=4'),
+  )
+),
+2=>array (
+  'name'=>'dataviz_contribute',
+  'entity'=> 'Dashboard',
+  'params'=> array (
+    'version'=>'3',
+    'name'=>'dataviz_contribute',
+    'label'=> 'Dataviz of contributions',
+    'url' => CRM_Utils_System::url('civicrm/dataviz/contribute', 'snippet=4'),
+  )
+)
+
+);
