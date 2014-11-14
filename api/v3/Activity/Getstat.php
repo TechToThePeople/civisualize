86cab608be1df2293e82ea8f893b241a5687ddf7
Tim Otten <totten@civicrm.org>
2014-11-13 19:22:45 -0800

    api/v3/*/Getstat - Fix duplication function names
    There are three functions named civicrm_api3_group_contact_getstat(); this smells
    like a copy-paste error.

--- api/v3/Activity/Getstat.php
+++ api/v3/Activity/Getstat.php
@@ -1,5 +1,5 @@
 <?php
-function civicrm_api3_group_contact_getstat ($params) {
+function civicrm_api3_activity_getstat ($params) {
   $sql="SELECT COUNT(id),DATE(register_date) as register from civicrm_participant group by DATE(register_date)";
   return _civicrm_api3_basic_getsql ($params,$sql);
 }
