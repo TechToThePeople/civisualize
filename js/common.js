2106728c04cd4c31d624b120e5111eb75d4a16d8
xavier <github@tttp.eu>
2017-04-17 10:23:57 +0200

    make the help button right clickable

--- js/common.js
+++ js/common.js
@@ -1,7 +1,8 @@
 jQuery(function($){
-  $(".menu.secondary").append('<li class="civisualize-help"><a href="#" class="glyphicon glyphicon-question-sign" aria-hidden="true"></a></li>');
-  $(".civisualize-help").click(function(){
-    var url=window.location.href.replace("dataviz","datadoc");
+  var url=window.location.href.replace("dataviz","datadoc");
+  $(".menu.secondary").append('<li class="civisualize-help"><a href="'+url+'" title="Help" class="glyphicon glyphicon-question-sign" aria-hidden="true"></a></li>');
+  $(".civisualize-help").click(function(event){
+    event.preventDefault();
     if (typeof marqued == "function")
       CRM.loadPage(url);
     else 
