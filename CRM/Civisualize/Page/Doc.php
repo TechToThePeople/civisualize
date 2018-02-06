c70f7548374380444101aefa50a68b070c6d0c2f
xavier dutoit <xavier@sydesy.com>
2018-02-06 16:27:06 +0100

    add link to modify the doc files on github

--- CRM/Civisualize/Page/Doc.php
+++ CRM/Civisualize/Page/Doc.php
@@ -16,24 +16,35 @@ class CRM_Civisualize_Page_Doc extends CRM_Core_Page {
     $tplfile = NULL;
     $smarty= CRM_Core_Smarty::singleton( );
     $smarty->assign("options",array());
+    $path = "doc/";
     if (CRM_Utils_Array::value(2, $request)) {
       $tplfile = _civicrm_api_get_camel_name($request[2]);
       $tplfile = explode('?', $tplfile);
-      $mdfile = 'doc/'.$tplfile[0].'.md';
+      $file = $tplfile[0].'.md';
     }
     if (CRM_Utils_Array::value(3, $request)) {
       $r3 = _civicrm_api_get_camel_name($request[3]);
-      if (!is_numeric($r3))
-        $mdfile = 'doc/'.$tplfile[0].'/'.$r3 .'.md';
+      if (!is_numeric($r3)) {
+        $file = $r3 .'.md';
+        $path = $path.$tplfile[0].'/';
+      }
       $smarty->assign("id",$r3);
     }
-
-    $md = file_get_contents($mdfile, FILE_USE_INCLUDE_PATH);
+    
+    if (CIVISUALIZE_GITHUB_DOC) {
+      $smarty->assign("github",CIVISUALIZE_GITHUB_DOC);
+    } else {
+      $smarty->assign("github","TechToThePeople/civisualize");
+    }
+    
+//    $github = civicrm_api3('setting', 'getvalue', array('group' => 'Civisualize Preferences', 'name' => 'civisualize_github_doc'));
+    $md = file_get_contents($path.$file, FILE_USE_INCLUDE_PATH);
     if (!$md) {
-      $md= "$mdfile not found"; 
+      $md= "$path$file not found"; 
       $smarty->assign("error","missing_file");
     }
-    $smarty->assign("mdfile",$mdfile);
+    $smarty->assign("mdfile",$file);
+    $smarty->assign("path",$path);
     $smarty->assign("md",$md);
 
     if (CRM_Utils_Array::value(4, $request)) {
--- templates/CRM/Civisualize/Page/Doc.tpl
+++ templates/CRM/Civisualize/Page/Doc.tpl
@@ -14,8 +14,10 @@ jQuery(function($){
 
 </div>
 {if $error}
-<div>
-To add it, create a {$mdfile} and write the help you want (in markdown format).
+<div class="alert alert-danger" role="alert">
+  <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+  <span class="sr-only">Error:</span>
+To add it, create a <a href="https://github.com/{$github}/new/master/{$path}?filename={$mdfile}">{$mdfile}</a> and write the help you want (in markdown format).
 </div>
 {/if}
-<div>&nbsp;</div><div class="small pull-right">file: {$mdfile}</div>
+<div>&nbsp;</div><div class="small pull-right">file: <a href="https://github.com/{$github}/blob/master/{$path}{$mdfile}">{$mdfile}</a></div>
