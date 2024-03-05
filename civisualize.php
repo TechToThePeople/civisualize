6ee4cd7f741ba1720d91a825e325b2764ba6584e
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2024-03-05 17:20:44 +0000

    civix upgrade for smarty 3

--- civisualize.php
+++ civisualize.php
@@ -14,7 +14,6 @@ function _civicrm_api3_basic_getsql ($params,$sql) {
   return civicrm_api3_create_success($values, $params, NULL, NULL, $dao);
 }
 
-
 require_once 'civisualize.civix.php';
 
 function civisualize_civicrm_dashboard( $contactID, &$contentPlacement ) {
@@ -28,7 +27,6 @@ function civisualize_civicrm_dashboard( $contactID, &$contentPlacement ) {
     ->addStyleFile('eu.tttp.civisualize', 'css/style.css');
    */
 }
-
 
 /**
  * Implementation of hook_civicrm_config
@@ -49,16 +47,6 @@ function civisualize_civicrm_container($container) {
             ->addMethodCall('addListener', [ 'hook_civicrm_buildAsset', ['\CRM_Civisualize_VisualBundle', 'buildAssetCss']]);
 }
 
-
-/**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- */
-function civisualize_civicrm_xmlMenu(&$files) {
-  _civisualize_civix_civicrm_xmlMenu($files);
-}
-
 /**
  * Implementation of hook_civicrm_install
  */
@@ -67,47 +55,10 @@ function civisualize_civicrm_install() {
 }
 
 /**
- * Implementation of hook_civicrm_uninstall
- */
-function civisualize_civicrm_uninstall() {
-  return _civisualize_civix_civicrm_uninstall();
-}
-
-/**
  * Implementation of hook_civicrm_enable
  */
 function civisualize_civicrm_enable() {
   return _civisualize_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- */
-function civisualize_civicrm_disable() {
-  return _civisualize_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function civisualize_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _civisualize_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implementation of hook_civicrm_managed
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- */
-function civisualize_civicrm_managed(&$entities) {
-  return _civisualize_civix_civicrm_managed($entities);
 }
 
 /**
